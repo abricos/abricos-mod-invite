af52cb13cf8d1885bbcc6f5401226eddfa151761
Alexander Kuzmin <roosit@abricos.org>
2013-10-11 12:28:56 +0400

    возможность создавать виртуальных пользователей

--- includes/manager.php
+++ includes/manager.php
@@ -104,6 +104,15 @@ class InviteManager extends Ab_ModuleManager {
 	
 	/**
 	 * Зарегистрировать пользователя по приглашению
+	 *
+	 * Если пользователь виртуальный, то его можно будет пригласить позже.
+	 * Виртаульный пользователь необходим для того, чтобы можно было работать с 
+	 * его учеткой как с реальным пользователем. 
+	 * Допустим, создается список сотрудников компании. 
+	 * Выяснять их существующие емайлы или регить новые - процесс длительный,
+	 * а работать в системе уже нужно сейчас. Поэтому сначало создается виртуальный
+	 * пользователь, а уже потом, если необходимо, он будет переводиться в статус реального
+	 * с формированием пароля и отправкой приглашения.
 	 * 
 	 * Коды ошибок:
 	 * 	1 - неверный емайл;
@@ -116,29 +125,38 @@ class InviteManager extends Ab_ModuleManager {
 	 * @param string $email
 	 * @param string $firstname
 	 * @param string $lastname
+	 * @param boolean $isVirtual True-виртуальный пользователь
 	 */
-	public function UserRegister($modname, $email, $firstname, $lastname){
+	public function UserRegister($modname, $email, $firstname, $lastname, $isVirtual = false){
 		if (!$this->IsWriteRole()){ return null; }
+		
+		if ($isVirtual){ // виртуальному пользователю емайл не нужен
+			$email = '';
+		}
 		
 		$ret = new stdClass();
 		$ret->error = 0;
-		
-		$manUser = Abricos::$user->GetManager();
-		$email = strtolower($email);
-		if (!$manUser->EmailValidate($email)){
-			$ret->error = 1;
-			return $ret;
-		}
-		
+
+		// приглашающий пользователь должен иметь имя и фамилию
+		// иначе как приглашенный пользователь поймет кто его пригласил?
 		if (empty($this->user->info['firstname']) || empty($this->user->info['lastname'])){
 			$ret->error = 4;
 			return $ret;
 		}
 		
-		$uinfo = InviteQuery::UserByEmailInfo($this->db, $email);
-		if (!empty($uinfo)){
-			$ret->error = 3;
-			return $ret;
+		$manUser = Abricos::$user->GetManager();
+		if (!$isVirtual){
+			// приглашение реального пользователя
+			$email = strtolower($email);
+			if (!$manUser->EmailValidate($email)){
+				$ret->error = 1; // емайл указан не верно
+				return $ret;
+			}
+			$uinfo = InviteQuery::UserByEmailInfo($this->db, $email);
+			if (!empty($uinfo)){
+				$ret->error = 3; // уже есть пользователь с таким емайл
+				return $ret;
+			}
 		}
 		
 		$fnmLat = translateruen($firstname);
@@ -147,9 +165,8 @@ class InviteManager extends Ab_ModuleManager {
 		$lnmLat = translateruen($lastname);
 		$lnmLat = strtoupper(substr($lnmLat, 0, 1)).substr($lnmLat, 1);
 		
-		
 		if(empty($firstname) || empty($lastname) || empty($fnmLat) || empty($lnmLat)){
-			$ret->error = 2;
+			$ret->error = 2; // имя и фамилия пользователя неверны
 			return $ret;
 		}
 		
@@ -169,16 +186,17 @@ class InviteManager extends Ab_ModuleManager {
 			$uinfo = UserQueryExt::UserByName($this->db, $login);
 		}
 		
-		if (!empty($uinfo)){ // уже есть такой логин, может по мылу свободно?
-			$arr = explode("@", $email);
-			$login = $arr[0];
-			$uinfo = UserQueryExt::UserByName($this->db, $login);
+		if (!$isVirtual){
+			if (!empty($uinfo)){ // уже есть такой логин, может по мылу свободно?
+				$arr = explode("@", $email);
+				$login = $arr[0];
+				$uinfo = UserQueryExt::UserByName($this->db, $login);
+			}
 		}
 		
 		if (!empty($uinfo)){ // следующий вариант  И+Фамилия+номерпользователя
-			
 			$cnt = InviteQuery::UserCount($this->db);
-			for ($i=$cnt;$i<($cnt+100);$i++){
+			for ($i=$cnt;$i<($cnt+300);$i++){
 				$login = substr($fnmLat, 0, 1).$lnmLat.$i;
 				$uinfo = UserQueryExt::UserByName($this->db, $login);
 				if (empty($uinfo)){
@@ -204,7 +222,7 @@ class InviteManager extends Ab_ModuleManager {
 		$user["email"] = $email;
 		
 		// Добавление пользователя в базу
-		$userid = UserQueryExt::UserAppend($this->db, $user, User::UG_REGISTERED);
+		$userid = UserQueryExt::UserAppend($this->db, $user, User::UG_REGISTERED, '', false, $isVirtual);
 		InviteQuery::UserSetFLName($this->db, $userid, $firstname, $lastname);
 		
 		$pubkey = md5(TIMENOW.$login.$password);
@@ -214,7 +232,10 @@ class InviteManager extends Ab_ModuleManager {
 		$ret->user['password'] = $password;
 		$ret->user['pubkey'] = $pubkey;
 		
-		InviteQuery::InviteAppend($this->db, $modname, $userid, $this->userid, $pubkey);
+		if (!$isVirtual){
+			// добавить инвайт в базу
+			InviteQuery::InviteAppend($this->db, $modname, $userid, $this->userid, $pubkey);
+		}
 		
 		return $ret;
 	}
